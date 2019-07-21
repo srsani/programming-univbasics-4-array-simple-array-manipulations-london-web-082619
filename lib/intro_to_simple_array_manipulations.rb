b96faa2aa5d876e0b99f7b74a6a3c21e721764b3
droll-accelerator-1483 <srsani@gmail.com>
2019-07-21 10:22:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,7 @@ def using_concat(array1,array2)
   array1.concat(array2)
 end
 
-def using_insert (array, elem),
+def using_insert (array, element),
 
 def using_uniq(array)
   array_out = array.uniq()
